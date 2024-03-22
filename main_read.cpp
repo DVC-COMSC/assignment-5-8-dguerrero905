e3c84aec3fb77eec3cd8857745d3a469af54c2ab
dguerrero905 <158696989+dguerrero905@users.noreply.github.com>
2024-03-21 20:29:28 -0700

    Update main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-int main()
-{
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+int main() {
+    int num;
+    int sum = 0;
+
+    // Prompt the user to input a number
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Validate if the input number is positive
+    if (num <= 0) {
+        cout << "Please enter a positive number." << endl;
+        return 1; // Exit the program with an error code
+    }
+
+    // Calculate the summation of the series
+    for (int i = 1; i <= num; ++i) {
+        sum += i;
+    }
+
+    // Print the summation of the series
+    cout << "The summation of the series up to " << num << " is: " << sum << endl;
+
+    return 0;
 }
